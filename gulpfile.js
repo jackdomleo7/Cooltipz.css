4f59d0b5fdffa33d6f5c19da383a94260083ad4d
Jack Domleo <jackdomleo7@hotmail.com>
2020-04-16 17:09:51 +0100

    Added gulp to compress SCSS, add vendor prefixes and add a sourcemap

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const {src, dest, parallel} = require('gulp');
+const autoVendorPrefix = require('gulp-autoprefixer');
+const gulpSass = require('gulp-sass');
+const purge = require('gulp-css-purge');
+const rename = require('gulp-rename');
+const sourcemaps = require('gulp-sourcemaps');
+
+function cooltipzCompressed() {
+  return src('./src/cooltipz/cooltipz.scss')
+      .pipe(sourcemaps.init())
+      .pipe(gulpSass().on('error', gulpSass.logError))
+      .pipe(purge({
+        trim: true,
+        shorten: true,
+        format: true
+      }))
+      .pipe(autoVendorPrefix())
+      .pipe(rename('cooltipz.min.css'))
+      .pipe(sourcemaps.write())
+      .pipe(dest('./docs'))
+}
+
+exports.default = parallel(cooltipzCompressed);
